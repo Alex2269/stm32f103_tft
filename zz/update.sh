9adde262ddc2e58a54e6002eac42edc92698c13b
Alex <alekader@rambler.ru>
2020-01-10 20:43:22 +0200

    2020-01-10

---
+++ zz/update.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+   #******************************************
+   # echo "# stm32f103_tft" >> README.md
+   git init
+   git status
+   git add .
+   git commit -m "$(date "+%Y-%m-%d")"
+   git remote add origin git@github.com:Alex2269/stm32f103_tft.git
+   git push -u origin master
+   git pull
+   #******************************************
+
+   #******************************************
+   git status
+   git add .
+   git commit -m "$(date "+%Y-%m-%d")"
+   git push -u origin master
+   git pull
+   #******************************************
+
